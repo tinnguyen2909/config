8841ab389d6ae288ade3b174973360fce1113ac2
Tin <tin@olli-ai.com>
2025-09-06 07:17:22 +0700

    add aliases to source virtual env and load env vars

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,2 +1,4 @@
 # source python virtual environment
 alias senv="if [ -d .venv ]; then source .venv/bin/activate; else source venv/bin/activate; fi"
+alias loadenv='[ -f .env ] && { set -a; source .env 2>/dev/null; set +a; echo ".env loaded."; } || echo "No .env file found."'
+alias loadenv_dev='[ -f .dev.env ] && { set -a; source .dev.env 2>/dev/null; set +a; echo ".dev.env loaded."; } || echo "No .dev.env file found."'
