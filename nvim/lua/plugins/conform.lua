128c01a9c7d87a1310fe5291bedab093ea41c2fe
Tin <tin@olli-ai.com>
2025-09-07 10:19:52 +0700

    add formatter for js, ts

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,6 +4,16 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "ruff_format" },
+			javascript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescript = { "prettier" },
+			typescriptreact = { "prettier" },
+			json = { "prettier" },
+			jsonc = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			scss = { "prettier" },
+			markdown = { "prettier" },
 		},
 		format_on_save = {
 			timeout_ms = 500,
