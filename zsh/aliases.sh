b7b2c7e15961c8fc823e29e44c92e8926de77f6c
Tin <tin@olli-ai.com>
2025-11-19 16:39:52 +0700

    add tcopy to copy tmux buffer

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -16,3 +16,6 @@ find_and_source_venv() {
 find_and_source_venv'
 alias loadenv='[ -f .env ] && export $(grep -v "^#" .env | grep -v "^$" | xargs) || echo ".env file not found"'
 alias loadenv_dev='[ -f .env.dev ] && export $(grep -v "^#" .env.dev | grep -v "^$" | xargs) || echo ".env.dev file not found"'
+tcopy() {
+  tmux show-buffer | xsel -ib
+}
